4d0a7203e62956f5d001288ffa940b49606a4024
Arnav Surve <arnavsurve@gmail.com>
2024-07-08 14:28:28 -0700

    feat: Add Tailwind CSS configuration and basic navbar function

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { page } from '$app/stores'; // import $page store from SvelteKit
+    const links = [
+        { text: "home", href: "/" },
+        { text: "about", href: "/about" },
+        { text: "projects", href: "/projects" },
+    ];
+
+    $: currentPath = $page.url.pathname;
+</script>
+
+<nav>
+    <ul class="font-mono">
+        {#each links as { href, text }}
+            <li><a href={href}>{href === currentPath ? '* ':''}{text}</a></li>
+        {/each}
+    </ul>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem;
+    }
+
+    /* ul {
+        margin-left: auto;
+    } */
+
+    li:hover {
+        text-decoration: underline;
+    }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Navbar from "../components/Navbar.svelte";
+</script>
+
+<h1 class="font-mono text-slate-500">arnav surve</h1>
+
+<Navbar />
+
+<style>
+    h1 {
+        padding: 1rem;
+    }
+</style>
