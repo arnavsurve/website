44a718f6d288cb01f01759920d179ff069c2ebe8
Arnav Surve <arnavsurve@gmail.com>
2024-07-12 11:16:43 -0700

    feat: Add resume page with header and resume components

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Header from "../../components/Header.svelte";
+    import Resume from "../../components/Resume.svelte";
+</script>
+
+<Header />
+
+<div class="m-4 md:w-3/5 h-screen">
+    <Resume />
+</div>
