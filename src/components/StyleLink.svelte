9e6e0cc7b9e3a069b6a991c45202573278cd67c8
Arnav Surve <arnavsurve@gmail.com>
2024-07-12 11:57:21 -0700

    feat: Add StyleLink component for inline styled links

---
+++ src/components/StyleLink.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let url = "#";
+    export let classDefs= "";
+</script>
+
+<div class={"inline " + classDefs}>
+    <a class="text-slate-500 hover:underline font-mono" href={url}>
+        <slot></slot>
+    </a>
+</div>
