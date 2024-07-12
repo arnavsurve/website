ad0ba374acc5092799065850a69b380b59492355
Arnav Surve <arnavsurve@gmail.com>
2024-07-12 11:57:31 -0700

    feat: Update projects page with personal portfolio information

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,22 @@
 <script>
     import Header from '../../components/Header.svelte';
     import Prose from '../../components/Prose.svelte';
+    import StyleLink from '../../components/StyleLink.svelte';
 </script>
 
 <Header />
 
-<Prose>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit accusantium magnam delectus necessitatibus. Repellendus neque exercitationem laudantium pariatur ipsa assumenda, facilis, ex qui iusto ut labore ab placeat cum soluta.
-</Prose>
+<div class="m-4 font-mono">
+    <StyleLink classDefs="text-lg" url="https://github.com/arnavsurve/website">
+        this website - july 2024
+    </StyleLink>
+
+    <Prose>
+        This is my personal portfolio built using SvelteKit as a frontend framework and TailwindCSS for styling components. The source code is available on GitHub at the link above, and I am using Vercel for deployment and hosting. The design of this website is inspired by  the <StyleLink url={"https://tinygrad.org/"}>tinygrad</StyleLink> website.
+    </Prose>
+
+
+
+
+</div>
+
