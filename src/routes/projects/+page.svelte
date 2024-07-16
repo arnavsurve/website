a8630e7efd6343d6e5c5a1dff504df108878c33b
Arnav Surve <arnavsurve@gmail.com>
2024-07-16 12:28:27 -0700

    refactor: Update projects page with personal portfolio information

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -12,11 +12,7 @@
     </StyleLink>
 
     <Prose>
-        This is my personal portfolio built using SvelteKit as a frontend framework and TailwindCSS for styling components. The source code is available on GitHub at the link above, and I am using Vercel for deployment and hosting. The design of this website is inspired by the <StyleLink url={"https://tinygrad.org/"}>tinygrad</StyleLink> website.
+        This is my personal portfolio built using SvelteKit for the frontend UI and TailwindCSS for styling components. The source code is available on GitHub at the link above, and I am using Vercel for deployment and hosting. The design of this website is inspired by the <StyleLink url={"https://tinygrad.org/"}>tinygrad</StyleLink> website.
     </Prose>
 
-
-
-
 </div>
-
