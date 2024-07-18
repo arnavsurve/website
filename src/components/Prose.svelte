8b34e81e521b7c5c6520f0a4df73000ac255d049
Arnav Surve <arnavsurve@gmail.com>
2024-07-18 15:40:14 -0700

    style: Adjust maximum width of Prose component for different screen sizes

--- src/components/Prose.svelte
+++ src/components/Prose.svelte
@@ -1,4 +1,9 @@
-<div class="max-w-full md:max-w-[80%] lg:max-w-[70%]">
+<script>
+    export let classDefs = "";
+</script>
+
+
+<div class={"max-w-full md:max-w-[80%] lg:max-w-[70%] " + classDefs}>
     <p class="font-mono text-sm">
         <slot></slot>
     </p>
