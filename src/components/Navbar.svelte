5506f9abcd4f72c5115d307effe9fc910853f3ca
Arnav Surve <arnavsurve@gmail.com>
2024-07-18 15:40:10 -0700

    style: Improve Navbar component with subpath navigation

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -8,12 +8,22 @@
     ];
 
     $: currentPath = $page.url.pathname;
+    $: pathSegments = currentPath.split('/').filter(Boolean); // Split the path and remove any empty strings
+    $: mainPath = '/' + (pathSegments[0] || ''); // Get the main path
+    $: subPath = pathSegments[1] || ''; // Get the subpath if any
 </script>
 
 <nav>
     <ul class="font-mono">
         {#each links as { href, text }}
-            <li><a href={href} class="hover:underline">{href === currentPath ? '* ':''}{text}</a></li>
+            <li>
+                <a href={href} class="hover:underline">{href === mainPath ? '* ':''}{text}</a>
+                {#if href === mainPath && subPath}
+                    <ul>
+                        <li><a href={currentPath} class="hover:underline pl-4"> ↳ {subPath}</a></li>
+                    </ul>
+                {/if}
+            </li>
         {/each}
     </ul>
 </nav>
