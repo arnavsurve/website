efbb402b72831f863415178a597eabc50e8eb072
Arnav Surve <arnavsurve@gmail.com>
2024-07-19 12:57:03 -0700

    feat: Add Codeblock component for syntax highlighting in Svelte

---
+++ src/components/Codeblock.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Prism from 'svelte-prism';
+    import 'prismjs/themes/prism-tomorrow.css';
+
+    let inlineCode = "";
+    let lang=""
+</script>
+
+<Prism language="{lang}" source={inlineCode}></Prism>
