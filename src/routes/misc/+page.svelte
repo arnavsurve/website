217f6d3aef9fd024cc37eb8841df6c8461c04919
Arnav Surve <arnavsurve@gmail.com>
2024-07-19 12:57:08 -0700

    Update misc page layout for better responsiveness

--- src/routes/misc/+page.svelte
+++ src/routes/misc/+page.svelte
@@ -1,21 +1,22 @@
 <script>
     import Header from '../../components/Header.svelte';
     import StyleLink from '../../components/StyleLink.svelte';
-    import LastFM from "../../components/LastFM.svelte";
+    import LastFM_top from "../../components/LastFM_top.svelte";
+    import LastFM_recent from "../../components/LastFM_recent.svelte";
 </script>
 
 <Header />
 
-<div class="m-4 font-mono sm:flex md:flex lg:flex justify-between">
-    <div class="m-4">
+<div class="font-mono sm:flex md:flex lg:flex justify-between">
+    <div class="mx-4">
         <h1 class="text-xl">hardware</h1>
         <ul class="text-sm list-disc p-2">
             <li>Macbook Pro (M2 Pro, 16GB RAM, 512GB SSD)</li>
             <li>AKG K371</li>
-            <li>Ducky One 2 (Cherry MX silent red)</li>
-            <li>Logitech G502 Mouse</li>
-            <li>Raspberry Pi 3b+</li>
             <li>Airpods Pro</li>
+            <li>Ducky One 2 (Cherry MX silent red)</li>
+            <li>Logitech G502</li>
+            <li>Raspberry Pi 3b+</li>
         </ul>
 
         <br>
@@ -27,15 +28,12 @@
             <li>zsh + oh-my-zsh</li>
             <li>tmux</li>
         </ul>
-
-        <br>
-
     </div>
 
     <br>
 
-    <div class="font-mono min-w-fit mx-2">
-        <h1 class="text-xl mb-2">my top 10 weekly (<StyleLink url="https://www.last.fm/user/arnavsurve">last.fm</StyleLink>)</h1>
-        <LastFM />
+    <div class="font-mono min-w-fit mx-4">
+        <h1 class="text-xl mb-2">recent listens (<StyleLink url="https://www.last.fm/user/arnavsurve">last.fm</StyleLink>)</h1>
+        <LastFM_recent />
     </div>
 </div>
