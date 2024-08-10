fe68096fd16ba37bb9067b822de1bec641b1a6dc
Arnav Surve <arnavsurve@gmail.com>
2024-08-10 01:59:56 -0700

    modified header styling

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,49 +1,56 @@
 <script>
-    import { onMount } from "svelte";
-    import Navbar from "./Navbar.svelte";
+	import { onMount } from 'svelte';
+	import Navbar from './Navbar.svelte';
 
-    onMount(() => {
-        const name = document.getElementById("name");
-        const text = "arnav surve";
-        let i = 0;
-        const type = () => {
-            if (i < text.length) {
-                // @ts-ignore
-                name.innerHTML += text.charAt(i);
-                i++;
-                setTimeout(type, 70);
-            }
-        };
-        type();
-    });
+	onMount(() => {
+		const name = document.getElementById('name');
+		const text = 'arnav surve';
+		let i = 0;
+		const type = () => {
+			if (i < text.length) {
+				// @ts-ignore
+				name.innerHTML += text.charAt(i);
+				i++;
+				setTimeout(type, 70);
+			}
+		};
+		type();
+	});
 </script>
 
-<div class="flex items-center justify-between font-mono p-4">
-    <h1 id="name" class="text-slate-500 text-3xl blinking-cursor"> </h1>
+<div class="font-mono p-4">
+	<div>
+		<h1 id="name" class="text-slate-500 text-3xl blinking-cursor justify-start"></h1>
+	</div>
 
-    <div class="justify-end">
-        <a class="hover:underline" href="mailto:arnavsurve@gmail.com">arnavsurve@gmail.com</a>
-        <a class="hover:underline" href="https://www.github.com/arnavsurve">github</a>
-        <a class="hover:underline" href="https://www.linkedin.com/in/arnavsurve/">linkedin</a>
-        <!-- <a href="https://leetcode.com/u/arnavsurve/">leetcode</a> -->
-    </div>
+	<div class="w-full py-2">
+		<a class="hover:underline" href="mailto:arnav@surve.dev">arnav@surve.dev</a>
+		<a class="hover:underline" href="https://www.github.com/arnavsurve">github</a>
+		<a class="hover:underline" href="https://www.linkedin.com/in/arnavsurve/">linkedin</a>
+	</div>
 </div>
 
+<hr class="mx-4" />
 <Navbar />
 
 <style>
-    @keyframes blink {
-        0%, 100% { opacity: 0; }
-        50% { opacity: 1; }
-    }
+	@keyframes blink {
+		0%,
+		100% {
+			opacity: 0;
+		}
+		50% {
+			opacity: 1;
+		}
+	}
 
-    .blinking-cursor::after {
-        content: '_';
-        animation: blink 1s step-start infinite;
-        font-weight: bold;
-    }
+	.blinking-cursor::after {
+		content: '_';
+		animation: blink 1.5s step-start infinite;
+		font-weight: bold;
+	}
 
-    h1 {
-        display: inline-block; /* Allows the cursor to appear right after the text */
-    }
+	h1 {
+		display: inline-block; /* Allows the cursor to appear right after the text */
+	}
 </style>
