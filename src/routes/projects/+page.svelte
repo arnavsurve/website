f84c6d6a11579a5122ead3c36d555dc760f102da
Arnav Surve <arnavsurve@gmail.com>
2024-08-10 02:00:52 -0700

    fixing typos

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -12,13 +12,12 @@
 	</StyleLink>
 
 	<Prose classDefs="p-2">
-		This is a version control system written in Go with the Cobra library for creating CLI
-		applications, using SHA1 hashes to track changes in files and directories. This project was
-		created as an exercise for Go development and for a deeper understanding of how version control
-		systems work. This is essentially a simplified version of Git with a subset of its features,
-		inspired by <StyleLink url="https://ryanheathcote.com/git/build-your-own-vcs"
-			>Build Your Own VCS</StyleLink
-		> by Ryan Heathcote. Code is hosted on GitHub at the link above.
+		A version control system written in Go with the Cobra library for creating CLI applications,
+		using SHA1 hashes to track changes in files and directories. This project was created as an
+		exercise in Go development and for a deeper understanding of how version control systems work.
+		This is essentially a simplified version of Git with a subset of its features, inspired by <StyleLink
+			url="https://ryanheathcote.com/git/build-your-own-vcs">Build Your Own VCS</StyleLink
+		> by Ryan Heathcote. Code hosted on GitHub at the link above.
 	</Prose>
 
 	<br />
@@ -30,26 +29,27 @@
 	<Prose classDefs="p-2">
 		HTML embed solutions for Last.fm stats suck - they are either not customizable, slow, or most of
 		the time both. I wanted to create a performant, customizable, and versatile solution for
-		embedding Last.fm stats on my website at <StyleLink url="/misc">/misc</StyleLink>. This is a
-		wrapper for the <StyleLink url="https://www.last.fm/home">last.fm</StyleLink> API written in Python
-		(Flask), containerized with Docker, and hosted with AWS Lambda for personal use. I implemented a
-		CI/CD pipeling with Zappa for seamless testing and deployment. The goal was to provide endpoints
-		that only returned JSON, allowing the developer to display data in the frontend as they wish. Code
-		hosted on GitHub at the link above.
+		embedding Last.fm stats on my website at <StyleLink url="/misc">/misc</StyleLink>; the goal
+		being to provide endpoints that returned raw metrics in JSON, allowing the developer to display
+		data in the frontend as they wish. This is a wrapper for the <StyleLink
+			url="https://www.last.fm/home">last.fm</StyleLink
+		> API written in Python (Flask), containerized with Docker, and hosted with AWS Lambda for personal
+		use. Additionally, I utilize Zappa to provide a CI/CD pipeline for seamless testing and deployment.
+		Code hosted on GitHub at the link above.
 	</Prose>
 
 	<br />
 
 	<div>
 		<StyleLink classDefs="text-xl" url="https://github.com/arnavsurve/website">
-			this website (july 2024)
+			surve.dev (july 2024)
 		</StyleLink>
 		<Prose classDefs="p-2">
-			This is my personal website built using SvelteKit for the frontend UI and TailwindCSS for
-			styling components. The source code is available on GitHub at the link above, and I am using
-			Vercel for deployment and hosting. The design of this website is inspired by the <StyleLink
-				url={'https://tinygrad.org/'}>tinygrad</StyleLink
-			> website.
+			My personal website built using SvelteKit for the frontend UI and TailwindCSS for styling
+			components and using Vercel to provide a CI/CD pipeline for deployment and hosting. The design
+			of this website is inspired by the <StyleLink url={'https://tinygrad.org/'}
+				>tinygrad</StyleLink
+			> website. Code hosted on GitHub at the link above.
 		</Prose>
 	</div>
 
