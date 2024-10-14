75f8e09c1bf25ee14933b588fb751f36b26f6781
Arnav Surve <arnav@surve.dev>
2024-10-13 23:50:56 -0700

    ts douchey

--- src/routes/misc/+page.svelte
+++ src/routes/misc/+page.svelte
@@ -9,18 +9,18 @@
 
 <div class="font-mono sm:flex md:flex lg:flex justify-between">
 	<div class="mx-4">
-		<h1 class="text-xl">hardware</h1>
-		<ul class="text-sm list-disc p-2">
-			<li>Macbook Pro (M2 Pro, 16GB RAM, 512GB SSD)</li>
-			<li>AKG K371</li>
-			<li>Airpods Pro</li>
-			<li>Ducky One 2 (Cherry MX silent red)</li>
-			<li>Logitech G502</li>
-			<li>Raspberry Pi 3b+</li>
-		</ul>
-
-		<br />
-
+		<!-- <h1 class="text-xl">hardware</h1> -->
+		<!-- <ul class="text-sm list-disc p-2"> -->
+		<!-- 	<li>Macbook Pro (M2 Pro, 16GB RAM, 512GB SSD)</li> -->
+		<!-- 	<li>AKG K371</li> -->
+		<!-- 	<li>Airpods Pro</li> -->
+		<!-- 	<li>Ducky One 2 (Cherry MX silent red)</li> -->
+		<!-- 	<li>Logitech G502</li> -->
+		<!-- 	<li>Raspberry Pi 3b+</li> -->
+		<!-- </ul> -->
+		<!---->
+		<!-- <br /> -->
+		<!---->
 		<h1 class="text-xl">software</h1>
 		<ul class="text-sm list-disc p-2">
 			<li>neovim (<StyleLink url="https://github.com/arnavsurve/nvim">my config</StyleLink>)</li>
