c823a22e99b86b72278c99be954ba8d159dac996
Arnav Surve <arnav@surve.dev>
2024-11-12 21:24:37 -0800

    update abt me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,28 +17,28 @@
 		<Prose>
 			I'm a third-year student at California State University, Sacramento pursuing a BS in Computer
 			Science. Passionate about everything software and a relentless advocate for open-source
-			software and the Linux community. Open to software development and sysadmin opportunities.
+			software and the Linux community. Open to SWE and Q/A opportunities.
 			<br /><br />
-			Linux sysadmin and DevOps engineering have been of recent key interest to me. I have been working
+			Server-side development and DevOps engineering have been of recent key interest to me. I have been working
 			through this <StyleLink url="https://roadmap.sh/devops">DevOps Roadmap</StyleLink> and studying
 			for the
 			<StyleLink url="https://aws.amazon.com/certification/certified-solutions-architect-associate/"
 				>AWS Certified Solutions Architect - Associate</StyleLink
 			> certification.
 			<br /><br />
-			Currently focused on developing a strong foundation in Go and Python programming as well as gaining
-			hands-on sysadmin experience with a homelab Ubuntu server environment on an old 2017 Macbook Pro.
-			I have ~2 years experience with both Debian (apt) and RHEL (dnf/yum) based linux distributions.
+			Currently focused on skilling up my Go programming as well as gaining
+			hands-on hardware sysadmin and CI/CD experience on a homelab setup.
+			I have ~2 years experience with Debian based linux distributions and have a CentOS box sitting somewhere in my closet as well.
 		</Prose>
 
 		<br />
 
 		<div class="font-mono">
-			<h1 class="text-xl">this summer I am:</h1>
+			<h1 class="text-xl">this summer I:</h1>
 			<ul class="text-sm list-disc p-2">
-				<li>Interning with MITER Brands IT Infrastructure in Tacoma, WA</li>
-				<li>Conducting infrasound research under the NASA California Space Grant Consortium</li>
-				<li>Taking MATH410 (Intro to Linear Algebra) at American River College</li>
+				<li>Interned with MITER Brands IT Infrastructure in Tacoma, WA</li>
+				<li>Conducted infrasound research under the NASA California Space Grant Consortium</li>
+				<li>Took MATH410 (Intro to Linear Algebra) at American River College</li>
 			</ul>
 		</div>
 	</div>
